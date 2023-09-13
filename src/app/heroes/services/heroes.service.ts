66604e709448682db01c003e3c5f0a9cf4807e0b
Luis Silva <luis.guerreros@elektra.com.mx>
2023-09-13 17:55:09 -0600

    avance

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Hero } from '../interfaces/hero.interface';
-import { Observable, catchError } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { environments } from 'src/environments/environments';
 
 @Injectable({ providedIn: 'root' })
@@ -18,6 +18,9 @@ export class HeroeService {
   getHeroById(id: string): Observable<Hero | undefined>{
 
       return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`)
+      .pipe(
+        catchError(error => of(undefined))
+      )
   }
 
 }
